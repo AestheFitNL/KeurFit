9c394344d50814c7f380fe2d2763f78cc17f2069
AestheFit <alihuseyinkumas@gmail.com>
2026-09-14 18:10:40 +0200

    Add sitemap function to generate site map

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://keurfit.nl",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1
+    }
+  ];
+}
