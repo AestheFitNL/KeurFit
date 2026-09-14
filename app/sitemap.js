626c9b0842c6be2a97a6de622327fd051b830d7f
AestheFit <alihuseyinkumas@gmail.com>
2026-09-14 18:44:00 +0200

    Refactor sitemap URLs to use baseUrl variable

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,30 @@
 export default function sitemap() {
+  const baseUrl = "https://keurfit.nl";
+
   return [
     {
-      url: "https://keurfit.nl",
+      url: `${baseUrl}/`,
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 1
-    }
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/rijbewijskeuring-nijmegen`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.95,
+    },
+    {
+      url: `${baseUrl}/75-plus-keuring`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/cbr-keuring`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
